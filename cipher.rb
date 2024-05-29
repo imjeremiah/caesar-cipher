7b633ba8723bb7fd7882d1a1c603326d1230fc3e
imjeremiah <52246955+imjeremiah@users.noreply.github.com>
2024-05-29 11:22:18 -0700

    Complete Caesar Cipher project

---
+++ cipher.rb
@@ -0,0 +1,19 @@
+def caesar_cipher(string, key)
+  cipher = []
+  alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 
+           "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 
+           "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 
+           "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",]
+  string_array = string.split("")
+  string_array.each do |item|
+    if item.match?(/[A-Za-z]/)
+      index = alpha.index(item)
+      cipher.push(alpha[index + key])
+    else
+      cipher.push(item)
+    end
+  end
+  p cipher.join("")
+end
+
+caesar_cipher("What a string!", 5) #"Bmfy f xywnsl!"
